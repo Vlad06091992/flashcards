632b8a18bd5e05399e446e25d7f5569ab9eb1f5c
Vlad <Smirnov.ru92@mail.ru>
2023-08-30 09:55:55 +0500

    Preparing for deployment to Vercel(6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 //Input, button, таблицы, карточка, пагинация  --- нативно, остальные на основе Radix
 
-import { Typography } from '../src/components/ui/typography/typography.tsx'
-
-import { LoginForm } from '@/components'
+import { LoginForm, Typography } from '@/components'
 
 export const App = () => {
   return (
