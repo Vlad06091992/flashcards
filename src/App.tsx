77412fef82bed5ed15b7b947b5f623b5f788d29b
Vlad <Smirnov.ru92@mail.ru>
2023-08-30 09:42:48 +0500

    Preparing for deployment to Vercel(4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //Input, button, таблицы, карточка, пагинация  --- нативно, остальные на основе Radix
 
-import { LoginForm, Button } from '@/components'
+import { LoginForm, Typography } from '@/components'
 
 export const App = () => {
   return (
     <div>
       <LoginForm />
-      <Button as={'p'}>fd;lg</Button>
+      <Typography color={'white'} variant={'large'}>
+        Card
+      </Typography>
     </div>
   )
 }
