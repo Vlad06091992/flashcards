891c8c3b1ff0480bb8569e362d419fd63e1ef6fe
Vlad <Smirnov.ru92@mail.ru>
2023-09-20 21:35:00 +0500

    create public and private routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,9 @@
-//Textfield, button, таблицы, карточка, пагинация  --- нативно, остальные на основе Radix
-
-import { Button, Textfield } from '@/components'
-
-const onSubmit = (data: any) => {
-  debugger
-}
+import { Router } from './router.tsx'
 
 export const App = () => {
   return (
     <div>
-      {/*<LoginForm />*/}
-      {/*<CheckEmail />*/}
-      {/*<SignUp onSubmit={onSubmit} />*/}
-      {/*<SignIn onSubmit={onSubmit} />*/}
-
-      {/*<RecoverPassword />*/}
-      {/*<CheckEmail email="Smirnov.ru92@mail.ru" />*/}
-      {/*<PersonalInformation*/}
-      {/*  email={'Smirnov.ru92@mail.ru'}*/}
-      {/*  name={'Ivan'}*/}
-      {/*  avatar={'https://i.pinimg.com/736x/94/ec/85/94ec85ebd3ddae955dcb28cfe7b23ccc.jpg'}*/}
-      {/*  onAvatarChange={() => {}}*/}
-      {/*  onLogout={() => {}}*/}
-      {/*  onNameChange={() => {}}*/}
-      {/*/>*/}
-      {/*<CheckboxComponent onChange={() => {}} label={'checkbox'} />*/}
-      {/*<CreateNewPassword onSubmit={onSubmit} />*/}
-      <div>
-        <Textfield
-          style={{ width: '300px' }}
-          variant={'search'}
-          onChange={e => console.log(e.currentTarget.value)}
-        />
-        <Button
-          onClick={() => {
-            debugger
-          }}
-        >
-          <p>click me</p>
-        </Button>
-      </div>
+      <Router />
     </div>
   )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,38 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject,
+  Outlet,
+  Navigate,
+} from 'react-router-dom'
 
-const router = createBrowserRouter([
+const PrivateRoutes = () => {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
   {
     path: '/',
     element: <div>hello</div>,
   },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+
+  ...publicRoutes,
 ])
 
 export const Router = () => {
