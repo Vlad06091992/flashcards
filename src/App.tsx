fc0f4c3a25c69f3f2adda140f19c8a109ea63255
Vlad <Smirnov.ru92@mail.ru>
2023-10-08 15:54:13 +0500

    update pagination component(3)

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export function App() {
     // </div>
 
     <div style={{ margin: '10px' }}>
-      <Pagination activePage={2} pagesQuality={55} />
+      <Pagination activePage={5} pagesQuality={55} />
     </div>
 
     // <Provider store={store}>
