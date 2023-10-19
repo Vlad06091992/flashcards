9fa41f608662c258094f36763cdf9b3f7f6a206d
Vlad <Smirnov.ru92@mail.ru>
2023-10-19 21:45:25 +0500

    create drop down component(base example)

--- src/components/ui/drop-down-menu/drop-down-menu.tsx
+++ src/components/ui/drop-down-menu/drop-down-menu.tsx
@@ -1,13 +1,25 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
+import s from './drop-down-menu.module.scss'
+
+import { AvatarIcon } from '@/components'
+
 export const DropDownMenuWithAvatar = () => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
         <button className="IconButton" aria-label="Customise options">
-          <HamburgerMenuIcon />
+          <AvatarIcon imageUrl={'https://a.d-cd.net/44IeiDnC6Jp8eHkQEFB9w-lCYQI-960.jpg'} />
         </button>
       </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content alignOffset={-5} align={'end'} className={s.dropDownMenuContent}>
+          <DropdownMenu.Item style={{ width: '200px' }}>1</DropdownMenu.Item>
+          <DropdownMenu.Item style={{ width: '100px' }}>2</DropdownMenu.Item>
+          <DropdownMenu.Item style={{ width: '100px' }}>3</DropdownMenu.Item>
+          <DropdownMenu.Arrow className={s.dropDownMenuArrow} />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
     </DropdownMenu.Root>
   )
 }
