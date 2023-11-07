62fa8d6053c5ecace6b2bf9ce7e328c6b4df3524
Vlad <Smirnov.ru92@mail.ru>
2023-11-07 18:26:43 +0500

    create show answer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AnswerRating } from '@/components/modals/answer rating/answer-rating.tsx'
 import { AddNewCard } from '@/components/modals/cards/add card/add-new-card.tsx'
 import { EditCard } from '@/components/modals/cards/edit deck/edit-card.tsx'
 import { DeleteEntity } from '@/components/modals/delete/delete-entity.tsx'
@@ -12,13 +13,14 @@ const questionFormat = [
 export function App() {
   return (
     <div style={{ margin: '10px' }}>
-      <DeleteEntity
-        cancelCallback={() => {}}
-        deleteCardCallback={() => {}}
-        closeCallback={() => {}}
-        title={'Delete Card'}
-        target={'Card'}
-        entities={'Cards'}
+      <AnswerRating
+        answerCount={10}
+        title={'Javascript'}
+        answer={'how are you'}
+        question={'good!'}
+        showAnswerCallback={() => {
+          console.log('show answer')
+        }}
       />
     </div>
   )
