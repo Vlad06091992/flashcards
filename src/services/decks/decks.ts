1ea522babad7c36b898fa3edeed031f375b5e6de
Vlad <Smirnov.ru92@mail.ru>
2023-12-22 19:42:45 +0500

    feat: create deck add  functional

--- src/services/decks/decks.ts
+++ src/services/decks/decks.ts
@@ -13,12 +13,16 @@ export const decksApi = baseApi.injectEndpoints({
         providesTags: ['Decks'],
       }),
 
-      createDeck: builder.mutation<Deck, { name: string }>({
-        query: body => ({
-          url: `v1/decks`,
-          method: 'POST',
-          body,
-        }),
+      createDeck: builder.mutation<Deck, any>({
+        query: body => {
+          debugger
+
+          return {
+            url: `v1/decks`,
+            method: 'POST',
+            body,
+          }
+        },
         invalidatesTags: ['Decks'],
       }),
     }
